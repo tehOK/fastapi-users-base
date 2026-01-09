fd5719be773b92bfed10c31d1f1505b9809a62da
tehOK <mishak62@gmail.com>
2026-01-09 13:07:53 +0300

    add db helper and create db

--- app/config.py
+++ app/config.py
@@ -10,8 +10,26 @@ class RunSettings(BaseModel):
     reload: bool = Field(default=True)
 
 
+class DBSettings(BaseModel):
+    db_url: str = Field(
+        default="sqlite+aiosqlite:///./test_db.sqlite3",
+    )
+    echo: bool = Field(
+        default=True,
+    )
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
+
 class Settings(BaseSettings):
     run: RunSettings = RunSettings()
+    db: DBSettings = DBSettings()
 
     model_config = ConfigDict(
         case_sensitive=False,
---
+++ app/core/models/db_helper.py
@@ -0,0 +1,39 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    create_async_engine,
+    async_sessionmaker,
+    AsyncSession,
+)
+
+from config import settings
+
+class DBHelper:
+    def __init__(
+        self,
+        url: bool,
+        echo: bool,
+    ) -> None:
+        self.engine: AsyncEngine = create_async_engine(
+            url=url,
+            echo=echo,
+        )
+        self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False,
+        )
+
+    async def dispose(self) ->None:
+        await self.engine.dispose()
+
+    async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_factory() as session:
+            yield session
+
+db_helper = DBHelper(
+    url=settings.db.db_url,
+    echo=settings.db.echo,
+)
