2821b375b205b9ba2a7e50c04004149bd7239220
tehOK <mishak62@gmail.com>
2026-01-09 14:47:31 +0300

    db strategies dependencies

---
+++ app/api/dependencies/strategies.py
@@ -0,0 +1,26 @@
+from typing import Annotated, TYPE_CHECKING
+
+from fastapi import Depends
+from fastapi_users.authentication.strategy.db import (
+    AccessTokenDatabase,
+    DatabaseStrategy,
+)
+
+from config import settings
+from .accesc_tokens import get_access_token_db
+
+if TYPE_CHECKING:
+    from fastapi_users.authentication.strategy.db import AccessTokenDatabase
+    from core.models import AccessToken
+
+
+def get_database_strategy(
+    access_token_db: Annotated[
+        "AccessTokenDatabase[AccessToken]",
+        Depends(get_access_token_db),
+    ],
+) -> DatabaseStrategy:
+    return DatabaseStrategy(
+        access_token_db,
+        lifetime_seconds=settings.access_token.lifetime_seconds,
+    )
